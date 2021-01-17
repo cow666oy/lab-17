f69419034aa6376c9e11fcca3e1325fee767f240
Nikita <nikita-kuznetsov-2018@list.ru>
2021-01-17 23:44:33 +0300

    17

--- 17/main.cpp
+++ 17/main.cpp
@@ -1,14 +1,108 @@
-//
-//  main.cpp
-//  17
-//
-//  Created by Никита Кузнецов on 17.01.2021.
-//
+Задание 1:
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include <Windows.h>
+using namespace std;
+int main() {
+int n, k, l;
+double avg = 0;
+cin >> n;
+int* a = new int[n];
+for (int i = 0; i < n; i++) {
+    cin >> a[i];
+}
+cin >> k >> l;
+for (int i = k; i <= l; i++) {
+    avg += a[i];
+}
+cout << avg / (l-k+1);
+}
 
-#include <iostream>
+2 задание:
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include <Windows.h>
+using namespace std;
+int main() {
+    bool flag = true;
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int r = a[1] - a[0];
+    for (int i = 2; i < n; i++) {
+        if (a[i] - a[i - 1] != r) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag == true) cout << "YES";
+    else cout << "NO";
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+3 задание:
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include <Windows.h>
+using namespace std;
+int main() {
+    int n, min = 2000007;
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i += 2) { //надеюсь что 0 тоже четный элемент
+        if (a[i] < min) min = a[i];
+    }
+    cout << min;
+}
+
+4 задание:
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include <Windows.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int MAX = max(a[n - 1], max(a[n - 2], a[n - 3]));
+    if (MAX == a[n - 1]) cout << n - 1;
+    else if (MAX == a[n - 2]) cout << n - 2;
+    else cout << n - 3;
+}
+
+5 задание:
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include <Windows.h>
+using namespace std;
+int main() {
+    int n, first = -1, second = -1;
+    cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (a[i] == a[j]) {
+                first = i;
+                second = j;
+                break;
+            }
+        }
+    }
+    cout << min(first, second) << " " << max(first, second);
 }
